3a20c6471bf1b39639e5457dd25184fe685ac651
Matthew Chivers <matthewchivers@outlook.com>
2024-04-10 09:25:47 +0000

    added calendar tool for working out current week

---
+++ caltools/caltools_test.go
@@ -0,0 +1,53 @@
+package caltools
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWeekOfMonth(t *testing.T) {
+	tests := []struct {
+		name string
+		date time.Time
+		want int
+	}{
+		{
+			name: "Week 1 - 1st March 2024 (Friday)",
+			date: time.Date(2024, time.March, 1, 0, 0, 0, 0, time.UTC),
+			want: 1,
+		},
+		{
+			name: "Week 1 - 3rd March 2024 (Sunday)",
+			date: time.Date(2024, time.March, 3, 0, 0, 0, 0, time.UTC),
+			want: 1,
+		},
+		{
+			name: "Week 2 - 4th March 2024 (Monday)",
+			date: time.Date(2024, time.March, 4, 0, 0, 0, 0, time.UTC),
+			want: 2,
+		},
+		{
+			name: "Week 5 - 25th March 2024 (Monday)",
+			date: time.Date(2024, time.March, 25, 0, 0, 0, 0, time.UTC),
+			want: 5,
+		},
+		{
+			name: "Week 5 - 31st March 2024 (Sunday)",
+			date: time.Date(2024, time.March, 31, 0, 0, 0, 0, time.UTC),
+			want: 5,
+		},
+		{
+			name: "Week 1 - 1st April 2024 (Monday)",
+			date: time.Date(2024, time.April, 1, 0, 0, 0, 0, time.UTC),
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := WeekOfMonth(tt.date)
+			assert.Equal(t, tt.want, got, "WeekOfMonth() = %v, want %v", got, tt.want)
+		})
+	}
+}

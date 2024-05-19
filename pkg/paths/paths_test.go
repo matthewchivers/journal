789163853b13382e87683ec2e7092610c17aa690
Matthew Chivers <matthewchivers@outlook.com>
2024-05-19 15:41:49 +0000

    updated names / corrected tests

--- pkg/paths/paths_test.go
+++ pkg/paths/paths_test.go
@@ -22,65 +22,65 @@ func TestParsePathTemplate(t *testing.T) {
 	wcCurrentMonth := weekCommencing.Format("01")
 
 	tests := []struct {
-		name            string
-		pathTemplate    string
-		docTemplateName string
-		want            string
-		wantErr         bool
+		name           string
+		pathTemplate   string
+		docDocTypeName string
+		want           string
+		wantErr        bool
 	}{
 		{
-			name:            "Valid template: year/month/day/",
-			pathTemplate:    "{{.Year}}/{{.Month}}/{{.Day}}/",
-			docTemplateName: "my-journal",
-			want:            currentYear + "/" + currentMonth + "/" + currentDay + "/",
-			wantErr:         false,
+			name:           "Valid template: year/month/day/",
+			pathTemplate:   "{{.Year}}/{{.Month}}/{{.Day}}/",
+			docDocTypeName: "my-journal",
+			want:           currentYear + "/" + currentMonth + "/" + currentDay + "/",
+			wantErr:        false,
 		},
 		{
-			name:            "Valid template: year/month/week/templateName/",
-			pathTemplate:    "{{.Year}}/{{.Month}}/{{.WeekNumber}}/{{.TemplateName}}/",
-			docTemplateName: "my-journal",
-			want:            currentYear + "/" + currentMonth + "/" + string(rune(currentWeek)) + "/my-journal/",
-			wantErr:         false,
+			name:           "Valid template: year/month/week/DocTypeName/",
+			pathTemplate:   "{{.Year}}/{{.Month}}/{{.WeekNumber}}/{{.DocTypeName}}/",
+			docDocTypeName: "my-journal",
+			want:           currentYear + "/" + currentMonth + "/" + string(rune(currentWeek)) + "/my-journal/",
+			wantErr:        false,
 		},
 		{
-			name:            "Valid template: year/month/wc weekCommencing/weekday/template/",
-			pathTemplate:    "{{.Year}}/{{.Month}}/wc {{.WeekCommencing}}/{{.WeekdayName}}/{{.TemplateName}}/",
-			docTemplateName: "my-journal",
-			want:            wcCurrentYear + "/" + wcCurrentMonth + "/wc " + weekCommencing.Format("2006-01-02") + "/" + currentWeekdayName + "/my-journal/",
-			wantErr:         false,
+			name:           "Valid template: year/month/wc weekCommencing/weekday/template/",
+			pathTemplate:   "{{.Year}}/{{.Month}}/wc {{.WeekCommencing}}/{{.WeekdayName}}/{{.DocTypeName}}/",
+			docDocTypeName: "my-journal",
+			want:           wcCurrentYear + "/" + wcCurrentMonth + "/wc " + weekCommencing.Format("2006-01-02") + "/" + currentWeekdayName + "/my-journal/",
+			wantErr:        false,
 		},
 		{
-			name:            "Valid template: year/month/week-commencing/template/",
-			pathTemplate:    "{{.Year}}/{{.Month}}/{{.WeekCommencing}}/{{.TemplateName}}/",
-			docTemplateName: "my-journal",
-			want:            currentYear + "/" + currentMonth + "/" + weekCommencing.Format("2006-01-02") + "/my-journal/",
-			wantErr:         false,
+			name:           "Valid template: year/month/week-commencing/template/",
+			pathTemplate:   "{{.Year}}/{{.Month}}/{{.WeekCommencing}}/{{.DocTypeName}}/",
+			docDocTypeName: "my-journal",
+			want:           currentYear + "/" + currentMonth + "/" + weekCommencing.Format("2006-01-02") + "/my-journal/",
+			wantErr:        false,
 		},
 		{
-			name:            "Path template without placeholders",
-			pathTemplate:    "static-path/my-journal.md",
-			docTemplateName: "ignored-template",
-			want:            "static-path/my-journal.md",
-			wantErr:         false,
+			name:           "Path template without placeholders",
+			pathTemplate:   "static-path/my-journal.md",
+			docDocTypeName: "ignored-template",
+			want:           "static-path/my-journal.md",
+			wantErr:        false,
 		},
 		{
-			name:            "Invalid path template",
-			pathTemplate:    "{{.Year}/{{.Month}}/{{.Day}}/{{.TemplateName}}/", // Missing a closing brace
-			docTemplateName: "my-journal",
-			want:            "",
-			wantErr:         true,
+			name:           "Invalid path template",
+			pathTemplate:   "{{.Year}/{{.Month}}/{{.Day}}/{{.DocTypeName}}/", // Missing a closing brace
+			docDocTypeName: "my-journal",
+			want:           "",
+			wantErr:        true,
 		},
 		{
-			name:            "Empty path template",
-			pathTemplate:    "",
-			docTemplateName: "my-journal",
-			want:            "",
-			wantErr:         false,
+			name:           "Empty path template",
+			pathTemplate:   "",
+			docDocTypeName: "my-journal",
+			want:           "",
+			wantErr:        false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := ParsePathTemplate(tt.pathTemplate, tt.docTemplateName)
+			got, err := ParsePathTemplate(tt.pathTemplate, tt.docDocTypeName)
 			if tt.wantErr {
 				assert.Error(t, err)
 				return

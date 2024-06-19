b0f412f7b88c8fe07109904f99e588acedae3b72
Matthew Chivers <matthewchivers@outlook.com>
2024-06-19 20:33:57 +0000

    added config validation

---
+++ pkg/config/validator_test.go
@@ -0,0 +1,120 @@
+package config
+
+import "testing"
+
+func TestValidateConfig(t *testing.T) {
+	type args struct {
+		cfg Config
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "base directory not set",
+			args: args{
+				cfg: Config{
+					Paths: Paths{
+						DirPattern: "{{.Year}}/{{.Month}}/{{.Day}}",
+					},
+					FileTypes: []FileType{
+						{
+							Name:          "foo",
+							FileExtension: "md",
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "directory pattern not set",
+			args: args{
+				cfg: Config{
+					Paths: Paths{
+						BaseDir: "/tmp",
+					},
+					FileTypes: []FileType{
+						{
+							Name:          "foo",
+							FileExtension: "md",
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "no file types defined",
+			args: args{
+				cfg: Config{
+					Paths: Paths{
+						BaseDir:    "/tmp",
+						DirPattern: "{{.Year}}/{{.Month}}/{{.Day}}",
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "file type name not set",
+			args: args{
+				cfg: Config{
+					Paths: Paths{
+						BaseDir:    "/tmp",
+						DirPattern: "{{.Year}}/{{.Month}}/{{.Day}}",
+					},
+					FileTypes: []FileType{
+						{
+							FileExtension: "md",
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "file extension not set",
+			args: args{
+				cfg: Config{
+					Paths: Paths{
+						BaseDir:    "/tmp",
+						DirPattern: "{{.Year}}/{{.Month}}/{{.Day}}",
+					},
+					FileTypes: []FileType{
+						{
+							Name: "foo",
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "successful validation",
+			args: args{
+				cfg: Config{
+					Paths: Paths{
+						BaseDir:    "/tmp",
+						DirPattern: "{{.Year}}/{{.Month}}/{{.Day}}",
+					},
+					FileTypes: []FileType{
+						{
+							Name:          "foo",
+							FileExtension: "md",
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ValidateConfig(tt.args.cfg); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateConfig() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}

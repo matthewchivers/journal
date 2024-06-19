1e630669b996d2fae1cd87db3e81f1c68d34cf35
Matthew Chivers <matthewchivers@outlook.com>
2024-06-19 20:33:57 +0000

    added config validation

---
+++ pkg/config/validator.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"errors"
+)
+
+func ValidateConfig(cfg Config) error {
+	if cfg.Paths.BaseDir == "" {
+		return errors.New("base directory not set")
+	}
+	if cfg.Paths.DirPattern == "" {
+		return errors.New("directory pattern not set")
+	}
+	if len(cfg.FileTypes) == 0 {
+		return errors.New("no file types defined")
+	}
+	for _, fileType := range cfg.FileTypes {
+		if fileType.Name == "" {
+			return errors.New("file type name not set")
+		}
+		if fileType.FileExtension == "" {
+			return errors.New("file extension not set")
+		}
+	}
+	return nil
+}

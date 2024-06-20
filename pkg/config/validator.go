e8f3779c48dff0fc5d1b260a823d5705b50abe07
Matthew Chivers <matthewchivers@outlook.com>
2024-06-20 19:38:04 +0000

    reduced cyclomatic complexity

--- pkg/config/validator.go
+++ pkg/config/validator.go
@@ -6,22 +6,40 @@ import (
 
 // ValidateConfig checks that the provided configuration is valid
 func ValidateConfig(cfg Config) error {
-	if cfg.Paths.BaseDir == "" {
+	if err := validatePaths(cfg.Paths); err != nil {
+		return err
+	}
+	if err := validateFileTypes(cfg.FileTypes, cfg.DefaultFileExtension); err != nil {
+		return err
+	}
+	return nil
+}
+
+// validatePaths checks that the paths in the configuration are valid
+func validatePaths(paths Paths) error {
+	if paths.BaseDir == "" {
 		return errors.New("base directory not set")
 	}
-	if cfg.Paths.DirPattern == "" {
+	if paths.DirPattern == "" {
 		return errors.New("directory pattern not set")
 	}
-	if len(cfg.FileTypes) == 0 {
+
+	return nil
+}
+
+// validateFileTypes checks that the file types in the configuration are valid
+func validateFileTypes(fileTypes []FileType, defaultFileExtension string) error {
+	if len(fileTypes) == 0 {
 		return errors.New("no file types defined")
 	}
-	for _, fileType := range cfg.FileTypes {
+	for _, fileType := range fileTypes {
 		if fileType.Name == "" {
 			return errors.New("file type name not set")
 		}
-		if fileType.FileExtension == "" && cfg.DefaultFileExtension == "" {
+		if fileType.FileExtension == "" && defaultFileExtension == "" {
 			return errors.New("file extension not set (defaultFileExtension must be set if fileExtension is not set for an individual file entry)")
 		}
 	}
+
 	return nil
 }

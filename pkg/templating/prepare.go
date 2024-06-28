9de726dc9090b27d6869bc675d8b5ac5a9a2a8f0
Matthew Chivers <matthewchivers@outlook.com>
2024-06-28 14:16:32 +0000

    made templating dates more robust - eliminates need for logic on week-commencing and adds lots more templating options

---
+++ pkg/templating/prepare.go
@@ -0,0 +1,129 @@
+package templating
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"time"
+
+	"github.com/matthewchivers/journal/pkg/caltools"
+)
+
+// PrepareTemplateData creates a new TemplateModel struct with the current date and file type
+func PrepareTemplateData(time time.Time) (TemplateModel, error) {
+	weekCommencing := caltools.WeekCommencing(time)
+
+	data := TemplateModel{
+		Date:   PopulateDate(time),
+		WCDate: PopulateDate(weekCommencing),
+	}
+	return data, nil
+}
+
+// PopulateDate creates a new Date struct with the current date
+func PopulateDate(time time.Time) Date {
+
+	// Get structs that will be filled
+	weekDay := getWeekday(time)
+	yearDay := getYearDay(time)
+	yearWeek := getYearWeek(time, weekDay)
+	month := getMonth(time, weekDay)
+
+	date := Date{
+		Year: Year{
+			Num:    time.Format("2006"),
+			Short:  time.Format("06"),
+			Month:  *month,
+			Week:   *yearWeek,
+			Day:    *yearDay,
+			DaysIn: fmt.Sprintf("%d", caltools.DaysInYear(time)),
+		},
+		Month: *month,
+		Day: WeekDay{
+			Num:   fmt.Sprintf("%d", time.Day()),
+			Pad:   fmt.Sprintf("%02d", time.Day()),
+			Ord:   fmt.Sprintf("%d%s", time.Day(), caltools.OrdinalSuffix(time.Day())),
+			Name:  time.Weekday().String(),
+			Short: time.Weekday().String()[:3],
+		},
+	}
+
+	return date
+}
+
+// ParsePattern creates a new path for a journal entry based on a path template
+func (tm *TemplateModel) ParsePattern(pattern string) (string, error) {
+	t, err := template.New("path").Parse(pattern)
+	if err != nil {
+		return "", err
+	}
+
+	var templateB bytes.Buffer
+	err = t.Execute(&templateB, tm)
+	if err != nil {
+		return "", err
+	}
+
+	parsedTemplate := templateB.String()
+
+	return parsedTemplate, nil
+}
+
+// getWeekday populates and returns a pointer to a WeekDay struct
+func getWeekday(time time.Time) *WeekDay {
+	weekday := &WeekDay{
+		Num:   fmt.Sprintf("%d", time.Weekday()),
+		Pad:   fmt.Sprintf("%02d", time.Weekday()),
+		Ord:   fmt.Sprintf("%d%s", time.Weekday(), caltools.OrdinalSuffix(int(time.Weekday()))),
+		Name:  time.Weekday().String(),
+		Short: time.Weekday().String()[:3],
+	}
+	return weekday
+}
+
+// getYearWeek populates and returns a pointer to a Week struct for the year
+func getYearWeek(time time.Time, weekDay *WeekDay) *Week {
+	_, yearWeekNum := time.ISOWeek()
+	week := &Week{
+		Num: fmt.Sprintf("%d", yearWeekNum),
+		Pad: fmt.Sprintf("%02d", yearWeekNum),
+		Ord: fmt.Sprintf("%d%s", yearWeekNum, caltools.OrdinalSuffix(yearWeekNum)),
+		Day: *weekDay,
+	}
+	return week
+}
+
+// getYearDay populates and returns a pointer to a Day struct for the year
+func getYearDay(time time.Time) *Day {
+	yearDayNum := time.YearDay()
+	yearDay := &Day{
+		Num: fmt.Sprintf("%d", yearDayNum),
+		Pad: fmt.Sprintf("%03d", yearDayNum),
+		Ord: fmt.Sprintf("%d%s", yearDayNum, caltools.OrdinalSuffix(yearDayNum)),
+	}
+	return yearDay
+}
+
+// getMonth populates and returns a pointer to a Month struct
+func getMonth(time time.Time, weekDay *WeekDay) *Month {
+	month := &Month{
+		Num:    time.Format("1"),
+		Pad:    time.Format("01"),
+		Ord:    fmt.Sprintf("%s%s", time.Format("1"), caltools.OrdinalSuffix(int(time.Month()))),
+		Name:   time.Month().String(),
+		Short:  time.Month().String()[:3],
+		DaysIn: fmt.Sprintf("%d", caltools.DaysInMonth(time)),
+		Day: Day{
+			Num: time.Format("2"),
+			Pad: time.Format("02"),
+			Ord: fmt.Sprintf("%s%s", time.Format("2"), caltools.OrdinalSuffix(time.Day())),
+		},
+		Week: Week{
+			Num: fmt.Sprintf("%d", caltools.WeekOfMonth(time)),
+			Pad: fmt.Sprintf("%02d", caltools.WeekOfMonth(time)),
+			Ord: fmt.Sprintf("%d%s", caltools.WeekOfMonth(time), caltools.OrdinalSuffix(caltools.WeekOfMonth(time))),
+			Day: *weekDay,
+		},
+	}
+	return month
+}
---
+++ pkg/templating/prepare_test.go
@@ -0,0 +1,126 @@
+package templating
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPopulateDate(t *testing.T) {
+	testTime := time.Date(2024, 6, 28, 0, 0, 0, 0, time.UTC)
+	date := PopulateDate(testTime)
+
+	assert.Equal(t, "2024", date.Year.Num)
+	assert.Equal(t, "24", date.Year.Short)
+	assert.Equal(t, "6", date.Month.Num)
+	assert.Equal(t, "28", date.Day.Num)
+}
+
+func TestParsePattern(t *testing.T) {
+	testTime := time.Date(2024, 6, 28, 0, 0, 0, 0, time.UTC)
+
+	type args struct {
+		entryID string
+		fileExt string
+		topic   string
+	}
+
+	tests := []struct {
+		name     string
+		args     args
+		pattern  string
+		expected string
+	}{
+		{
+			name:     "base",
+			args:     args{},
+			pattern:  "/journal/{{.Date.Year.Num}}/{{.Date.Month.Num}}/{{.Date.Day.Num}}",
+			expected: "/journal/2024/6/28",
+		},
+		{
+			name:     "week commencing",
+			pattern:  "/journal/{{.WCDate.Year.Num}}/{{.WCDate.Month.Num}}/{{.WCDate.Day.Num}}",
+			expected: "/journal/2024/6/24",
+		},
+		{
+			name: "entry id / other args",
+			args: args{
+				entryID: "foo",
+				fileExt: "md",
+				topic:   "bar",
+			},
+			pattern:  "/journal/{{.Date.Year.Num}}/{{.Date.Month.Num}}/{{.Date.Day.Num}}/{{.EntryID}}/{{.Topic}}.{{.FileExtension}}",
+			expected: "/journal/2024/6/28/foo/bar.md",
+		},
+		{
+			name:     "ordinals and other weird combinations",
+			pattern:  "/journal/{{.Date.Year.Num}}/{{.Date.Month.Num}}/{{.Date.Day.Num}}/{{.Date.Month.Ord}}/{{.Date.Year.Week.Ord}}/{{.Date.Day.Ord}}/{{.Date.Year.Day.Ord}}",
+			expected: "/journal/2024/6/28/6th/26th/28th/180th",
+		},
+		{
+			name:     "days in",
+			pattern:  "/journal/{{.Date.Year.Num}}/{{.Date.Month.Ord}} of 12 months/{{.Date.Year.Day.Ord}} day of {{.Date.Year.DaysIn}}",
+			expected: "/journal/2024/6th of 12 months/180th day of 366",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			templateData, _ := PrepareTemplateData(testTime)
+			templateData.EntryID = tt.args.entryID
+			templateData.FileExtension = tt.args.fileExt
+			templateData.Topic = tt.args.topic
+			parsedPath, err := templateData.ParsePattern(tt.pattern)
+			assert.NoError(t, err, "Error should be nil")
+			assert.Equal(t, tt.expected, parsedPath, "Parsed path should match expected path")
+		})
+	}
+}
+
+func TestGetWeekday(t *testing.T) {
+	testTime := time.Date(2024, 6, 28, 0, 0, 0, 0, time.UTC)
+	weekday := getWeekday(testTime)
+	assert.Equal(t, "5", weekday.Num)
+	assert.Equal(t, "05", weekday.Pad)
+	assert.Equal(t, "5th", weekday.Ord)
+	assert.Equal(t, "Friday", weekday.Name)
+	assert.Equal(t, "Fri", weekday.Short)
+}
+
+func TestGetYearWeek(t *testing.T) {
+	testTime := time.Date(2024, 6, 28, 0, 0, 0, 0, time.UTC)
+	weekDay := getWeekday(testTime)
+	yearWeek := getYearWeek(testTime, weekDay)
+
+	assert.Equal(t, "26", yearWeek.Num)
+	assert.Equal(t, "26", yearWeek.Pad)
+	assert.Equal(t, "26th", yearWeek.Ord)
+	assert.Equal(t, "5", yearWeek.Day.Num)
+	assert.Equal(t, "05", yearWeek.Day.Pad)
+	assert.Equal(t, "5th", yearWeek.Day.Ord)
+	assert.Equal(t, "Friday", yearWeek.Day.Name)
+	assert.Equal(t, "Fri", yearWeek.Day.Short)
+}
+
+func TestGetYearDay(t *testing.T) {
+	testTime := time.Date(2024, 6, 28, 0, 0, 0, 0, time.UTC)
+	yearDay := getYearDay(testTime)
+
+	assert.Equal(t, "180", yearDay.Num)
+	assert.Equal(t, "180", yearDay.Pad)
+	assert.Equal(t, "180th", yearDay.Ord)
+}
+
+func TestGetMonth(t *testing.T) {
+	testTime := time.Date(2024, 6, 28, 0, 0, 0, 0, time.UTC)
+	weekDay := getWeekday(testTime)
+	month := getMonth(testTime, weekDay)
+
+	assert.Equal(t, "6", month.Num)
+	assert.Equal(t, "06", month.Pad)
+	assert.Equal(t, "6th", month.Ord)
+	assert.Equal(t, "June", month.Name)
+	assert.Equal(t, "Jun", month.Short)
+	assert.Equal(t, "30", month.DaysIn)
+}

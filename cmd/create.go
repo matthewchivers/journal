0dd9163a2d0c159434740c39a9b738a18aa13390
Matthew Chivers <matthewchivers@outlook.com>
2024-07-10 08:13:45 +0000

    improved logging and var names

--- cmd/create.go
+++ cmd/create.go
@@ -9,24 +9,25 @@ import (
 )
 
 var (
-	entryIDParam       string
-	directoryPathParam string
-	FileExtParam       string
-	fileNameParam      string
-	topicParam         string
+	entryID       string
+	directoryPath string
+	fileExt       string
+	fileName      string
+	topic         string
 )
 
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "create a new journal entry",
 	Run: func(_ *cobra.Command, _ []string) {
-		log.Debug().Dict("flags_and_params", zerolog.Dict().
-			Str("entry_id", entryIDParam).
-			Str("directory", directoryPathParam).
-			Str("extension", FileExtParam).
-			Str("filename", fileNameParam).
-			Str("topic", topicParam),
-		).Msg("creating new journal entry with the 'create' command")
+		log.Debug().Dict("parameters", zerolog.Dict().
+			Str("entry_id", entryID).
+			Str("directory", directoryPath).
+			Str("extension", fileExt).
+			Str("filename", fileName).
+			Str("topic", topic)).
+			Str("command", "create").
+			Msg("creating new journal entry with the 'create' command")
 		if err := app.PreparePatternData(); err != nil {
 			log.Err(err).Msg("error preparing pattern data")
 			os.Exit(1)
@@ -58,23 +59,23 @@ var createCmd = &cobra.Command{
 }
 
 func init() {
-	createCmd.PersistentFlags().StringVar(&entryIDParam, "id", "", "entry ID to use for templating")
-	createCmd.PersistentFlags().StringVar(&directoryPathParam, "directory", "", "directory to create the file in")
-	createCmd.PersistentFlags().StringVar(&FileExtParam, "extension", "", "file extension to use")
-	createCmd.PersistentFlags().StringVar(&fileNameParam, "filename", "", "file name to use")
-	createCmd.PersistentFlags().StringVar(&topicParam, "topic", "", "topic to use for templating")
+	createCmd.PersistentFlags().StringVar(&entryID, "id", "", "entry ID to use for templating")
+	createCmd.PersistentFlags().StringVar(&directoryPath, "directory", "", "directory to create the file in")
+	createCmd.PersistentFlags().StringVar(&fileExt, "extension", "", "file extension to use")
+	createCmd.PersistentFlags().StringVar(&fileName, "filename", "", "file name to use")
+	createCmd.PersistentFlags().StringVar(&topic, "topic", "", "topic to use for templating")
 	rootCmd.AddCommand(createCmd)
 }
 
 // setTemplateDependencies sets the values for the template dependencies
 func setTemplateDependencies() error {
-	if err := app.SetEntryID(entryIDParam); err != nil {
+	if err := app.SetEntryID(entryID); err != nil {
 		return err
 	}
-	if err := app.SetTopic(topicParam); err != nil {
+	if err := app.SetTopic(topic); err != nil {
 		return err
 	}
-	if err := app.SetFileExt(FileExtParam); err != nil {
+	if err := app.SetFileExt(fileExt); err != nil {
 		return err
 	}
 	return nil
@@ -82,10 +83,10 @@ func setTemplateDependencies() error {
 
 // setTemplatedValues sets the values for the templated values
 func setTemplatedValues() error {
-	if err := app.SetFileName(fileNameParam); err != nil {
+	if err := app.SetFileName(fileName); err != nil {
 		return err
 	}
-	if err := app.SetDirectory(directoryPathParam); err != nil {
+	if err := app.SetDirectory(directoryPath); err != nil {
 		return err
 	}
 	return nil
